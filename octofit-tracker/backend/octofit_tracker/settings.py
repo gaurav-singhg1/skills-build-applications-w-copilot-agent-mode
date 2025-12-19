84d55c20d997aba92adce9b549bcff5da00bcae1
gaurav-singhg1 <Gaurav.Singh@hmhco.com>
2025-12-19 08:10:43 +0000

    Update settings and URLs to support codespace deployment and local development

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,18 @@ SECRET_KEY = 'django-insecure-@@&x7ymvj&i%_!7deb0dg+!2)dnt5gsc7qc#2cux&-)3%@us3x
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        'localhost',
+        '127.0.0.1',
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
